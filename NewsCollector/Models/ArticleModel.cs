298702d5306ae3d642c262a68be1118528d13871
Mateusz Rzeczkowski <mateusz.rzeczkowski96@gmail.com>
2018-05-14 21:03:03 +0200

    Po dodaniu modeli i wstępnej konfiguracji EF.

---
+++ NewsCollector/Models/ArticleModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NewsCollector.Models
+{
+    public class ArticleModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int AuthorId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string LeadingParagraph { get; set; }
+        [Required]
+        public string Body { get; set; }
+
+        //! Now its a path to the image, later will probably change
+        public string ImagePath { get; set; }
+
+    }
+}
