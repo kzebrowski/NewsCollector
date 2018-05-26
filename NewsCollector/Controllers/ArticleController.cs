9263f0c9227ff3b88e0a0dabb35c9e47bf5350e6
krzysztof-z <krzysiek2677@gmail.com>
2018-05-26 15:38:17 +0200

    Updating all nuget packages

--- NewsCollector/Controllers/ArticleController.cs
+++ NewsCollector/Controllers/ArticleController.cs
@@ -1,14 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using NewsCollector.Models;
-using NewsCollector.Models.Contexts;
-using Newtonsoft.Json;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using System.Web.Mvc;
 
 namespace NewsCollector.Controllers
 {
