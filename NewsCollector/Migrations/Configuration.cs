c78d0176cbb10d39b22df5390046023e46b06da6
MRzeczkowski <mateusz.rzeczkowski96@gmail.com>
2018-05-26 18:25:51 +0200

    Added migrations and some other not yet finished stuff

---
+++ NewsCollector/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace NewsCollector.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<NewsCollector.Models.Contexts.NewsContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(NewsCollector.Models.Contexts.NewsContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
