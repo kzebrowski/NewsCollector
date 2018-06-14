b999568edf34956b5ce3a84ef8226b1c491d9b45
aborze <taktoniema@gmail.com>
2018-06-14 18:53:08 +0200

    Look changes of subpages

--- NewsCollector/Models/AccountViewModels.cs
+++ NewsCollector/Models/AccountViewModels.cs
@@ -58,7 +58,7 @@ namespace NewsCollector.Models
         [Display(Name = "Password")]
         public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
+        [Display(Name = "Zapamiętaj mnie?")]
         public bool RememberMe { get; set; }
     }
 
